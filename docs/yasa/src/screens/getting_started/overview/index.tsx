a63c03dbf7a2397e466633139aa583d187c5f079
Yishay Hazan <yishay11013@gmail.com>
2023-08-17 11:31:50 +0300

    react live is cool

--- docs/yasa/src/screens/getting_started/overview/index.tsx
+++ docs/yasa/src/screens/getting_started/overview/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import { Button } from "@yasa/button";
+
+const scope = { Button };
+
+const code = `<Button>Hello</Button>`;
 
 export function GettingStartedOverviewScreen() {
   return (
     <>
       <h1>Getting Started</h1>
+
+      <pre>
+        <LiveProvider code={code} scope={scope}>
+          <LiveEditor />
+          <LiveError />
+          <LivePreview />
+        </LiveProvider>
+      </pre>
     </>
   );
 }
