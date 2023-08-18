1fd8c32d5ed6df1e79b5a882ab244f19ff47109e
Yishay Hazan <yishay11013@gmail.com>
2023-08-18 10:18:01 +0300

    Go

--- docs/yasa/src/screens/getting_started/installation/index.tsx
+++ docs/yasa/src/screens/getting_started/installation/index.tsx
@@ -1,7 +1,15 @@
+import { CodeBlock } from "docs/yasa/src/common/code_block";
+
 export function InstallationScreen() {
   return (
-    <>
+    <article>
       <h1>Installation</h1>
-    </>
+      <p>
+        In order to use Yasa, you will need to install it with npm.
+        <br />
+        Yasa uses Sass, so you will need to install it as well.
+      </p>
+      <CodeBlock code="npm i -D sass && npm i yasa" disabled noPreview />
+    </article>
   );
 }
--- docs/yasa/src/screens/getting_started/overview/index.tsx
+++ docs/yasa/src/screens/getting_started/overview/index.tsx
@@ -1,11 +1,28 @@
+import { Link } from "@yasa/link";
+
 export function GettingStartedOverviewScreen() {
   return (
     <>
-      <h1>Getting Started</h1>
-
+      <h1>Yasa</h1>
       <article>
+        <h2>Why</h2>
+        <p>
+          I like re-inventing the wheel.
+          <br />
+          And, I think Yasa introduces some unique features and concepts that
+          are very useful and time saving (<Link>Neutral colors</Link>,{" "}
+          <Link>Form</Link>, <Link>Filters</Link>).
+        </p>
         <h2>Inspiration</h2>
-        <p>Yasa is heavily inspired by bootstrap</p>
+        <p>
+          Yasa is heavily inspired by bootstrap, colors are inspired by the
+          design system "pink".
+        </p>
+        <h2>Limitations</h2>
+        <p>
+          I think it will be kinda hard to customize - I haven't really checked,
+          because I created this library to suite my needs.
+        </p>
       </article>
     </>
   );
